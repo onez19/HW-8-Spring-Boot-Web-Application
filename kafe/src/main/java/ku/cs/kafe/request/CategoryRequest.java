c8a5764a2277a896e623453111753fe6574a25db
Thanachote Ngamkana <gbb.tn@outlook.com>
2024-10-20 19:05:02 +0700

    Lab - Spring MVC & Thymeleaf 3 – One-To-Many ane Environment

---
+++ kafe/src/main/java/ku/cs/kafe/request/CategoryRequest.java
@@ -0,0 +1,16 @@
+package ku.cs.kafe.request;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+
+@Data
+public class CategoryRequest {
+
+
+  @NotBlank
+  private String name;
+
+
+}
