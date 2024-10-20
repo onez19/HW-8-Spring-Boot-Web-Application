cad777dd6cf51f03e52f47bf0e02450552c50b36
Thanachote Ngamkana <gbb.tn@outlook.com>
2024-10-20 18:15:05 +0700

    Lab - Spring MVC & Thymeleaf 2 – DTO, validation

--- kafe/src/main/java/ku/cs/kafe/controller/SignupController.java
+++ kafe/src/main/java/ku/cs/kafe/controller/SignupController.java
@@ -1,33 +1,38 @@
 package ku.cs.kafe.controller;
 
 
-import ku.cs.kafe.entity.Member;
+
+
+import ku.cs.kafe.request.SignupRequest;
 import ku.cs.kafe.service.SignupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.validation.Valid;
 
 @Controller
 public class SignupController {
 
-
   @Autowired
   private SignupService signupService;
 
-
   @GetMapping("/signup")
-  public String getSignupPage() {
+  public String getSignupPage(Model model) {
+    model.addAttribute("signupRequest", new SignupRequest());
     return "signup"; // return หน้าฟอร์ม signup.html
   }
 
 
   @PostMapping("/signup")
-  public String signupUser(@ModelAttribute Member user, Model model) {
-
+  public String signupUser(@Valid SignupRequest user,
+                           BindingResult result, Model model) {
+    if (result.hasErrors())
+      return "signup";
 
     if (signupService.isUsernameAvailable(user.getUsername())) {
       signupService.createUser(user);
@@ -35,7 +40,7 @@ public class SignupController {
     } else {
       model.addAttribute("signupError", "Username not available");
     }
-    // return หน้าฟอร์ม signup.html เช่นกัน แต่จะมี message ปรากฎ
+    model.addAttribute("signupRequest", new SignupRequest());
     return "signup";
   }
 }
